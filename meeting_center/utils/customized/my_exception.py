9faac8d9d384c31d37810da4574de888d944c091
TomNewChao <chaotomzhu@gmail.com>
2024-11-23 10:30:09 +0800

    fix authorization failed and return 403

--- meeting_center/utils/customized/my_exception.py
+++ meeting_center/utils/customized/my_exception.py
@@ -7,7 +7,7 @@
 
 from django.http import Http404
 from django.core.exceptions import PermissionDenied
-from rest_framework import exceptions
+from rest_framework import exceptions, status
 from rest_framework.response import Response
 from rest_framework.views import set_rollback
 from rest_framework_simplejwt.exceptions import AuthenticationFailed
@@ -32,12 +32,13 @@ def my_exception_handler(exc, context):
 
     if isinstance(exc, AuthenticationFailed):
         msg = RetCode.get_name_by_code(RetCode.STATUS_AUTH_FAILED)
+        resp_status = status.HTTP_401_UNAUTHORIZED
         data = {
             'msg': msg,
-            'code': exc.status_code,
+            'code': resp_status,
             'data': None
         }
-        return Response(data, status=exc.status_code)
+        return Response(data, status=resp_status)
 
     elif isinstance(exc, exceptions.APIException):
         headers = {}
