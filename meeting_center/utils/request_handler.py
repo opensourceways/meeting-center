753426849ca044876e535df35a76f56558f8743a
TomNewChao <chaotomzhu@gmail.com>
2024-11-25 11:49:11 +0800

    fix request the meeting-platform ssl mismatch error

--- meeting_center/utils/request_handler.py
+++ meeting_center/utils/request_handler.py
@@ -9,16 +9,17 @@ from requests.auth import HTTPBasicAuth
 
 
 class RequestHandler:
-    def __init__(self, username=None, password=None, timeout=None):
+    def __init__(self, username=None, password=None, timeout=None, verify=False):
         self.timeout = timeout if not timeout else 60
         if username and password:
             self._auth = HTTPBasicAuth(username, password)
         else:
             self._auth = None
+        self.verify = verify
 
     def get(self, url, cookies=None, headers=None, is_json=True, is_resp=False, is_suppress_error=True):
         resp = requests.get(url, auth=self._auth, cookies=cookies, headers=headers,
-                            timeout=(self.timeout, self.timeout))
+                            timeout=(self.timeout, self.timeout), verify=self.verify)
         if not str(resp.status_code).startswith("20") and not is_suppress_error:
             raise RuntimeError(
                 "get the url failed, return status is:{} the detail is:{}".format(resp.status_code, resp.content))
@@ -29,21 +30,23 @@ class RequestHandler:
         return resp.status_code, resp.content
 
     def post(self, url, json_data=None, data=None, is_suppress_error=True):
-        resp = requests.post(url, auth=self._auth, json=json_data, data=data, timeout=(self.timeout, self.timeout))
+        resp = requests.post(url, auth=self._auth, json=json_data, data=data,
+                             timeout=(self.timeout, self.timeout), verify=self.verify)
         if not str(resp.status_code).startswith("20") and not is_suppress_error:
             raise RuntimeError(
                 "post the url failed, return status is:{} the detail is:{}".format(resp.status_code, resp.content))
         return resp.status_code, resp.json()
 
     def put(self, url, json_data=None, data=None, is_suppress_error=True):
-        resp = requests.put(url, auth=self._auth, json=json_data, data=data, timeout=(self.timeout, self.timeout))
+        resp = requests.put(url, auth=self._auth, json=json_data, data=data,
+                            timeout=(self.timeout, self.timeout), verify=self.verify)
         if not str(resp.status_code).startswith("20") and not is_suppress_error:
             raise RuntimeError(
                 "put the url failed, return status is:{} the detail is:{}".format(resp.status_code, resp.content))
         return resp.status_code, resp.json()
 
     def delete(self, url, is_suppress_error=True):
-        resp = requests.delete(url, auth=self._auth, timeout=(self.timeout, self.timeout))
+        resp = requests.delete(url, auth=self._auth, timeout=(self.timeout, self.timeout), verify=self.verify)
         if not str(resp.status_code).startswith("20") and not is_suppress_error:
             raise RuntimeError(
                 "delete the url failed, return status is:{} the detail is:{}".format(resp.status_code, resp.content))
